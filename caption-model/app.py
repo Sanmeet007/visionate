a1e67e0d7f75352ca6e3cc58d32f79300d1ff435
Sanmeet007 <ssanmeet007@gmail.com>
2025-02-05 23:50:04 +0530

    add custom headers to bypass 403 Forbidden error for image URL request

--- caption-model/app.py
+++ caption-model/app.py
@@ -132,8 +132,14 @@ def generate_caption():
         image_url = request.json["image_url"]
 
         try:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                "Accept": "image/webp,*/*;q=0.8",  # Accept image/webp and other types
+                "Accept-Encoding": "gzip, deflate, br",
+                "Connection": "keep-alive",
+            }
 
-            image_response = requests.get(image_url)
+            image_response = requests.get(image_url, headers=headers)
             image_response.raise_for_status()
 
             uploaded_image = load_img(
