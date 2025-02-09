1e6dc5c29f804678e096ed8a3fa9e335a153d6d4
Sanmeet007 <ssanmeet007@gmail.com>
2025-02-09 23:58:33 +0530

    added generate caption

--- website/src/app/api/generate-caption/route.ts
+++ website/src/app/api/generate-caption/route.ts
@@ -1,43 +1,103 @@
+import { db } from "@/drizzle";
+import { apiKeysTable, usersTable } from "@/drizzle/schema";
 import { usingHasValidApiKeyMiddleware } from "@/middlewares/apikey-validator";
 import {
   getImageMetadataFromFile,
   getImageMetadataFromUrl,
 } from "@/utils/image-meta";
+import { eq } from "drizzle-orm";
 
 import { NextResponse } from "next/server";
 
-export const POST = usingHasValidApiKeyMiddleware(async (request , apikey) => {
-  try {
-    const formData = await request.formData();
+const getMaxAllowedImageSize = (userRole: string): number => {
+  switch (userRole) {
+    case "free":
+      break;
+    case "starter":
+      break;
+    case "pro":
+      break;
+    case "enterprise":
+      break;
+  }
 
+  return 0;
+};
+
+export const POST = usingHasValidApiKeyMiddleware(async (request, apikey) => {
+  try {
+    const [record] = await db
+      .select()
+      .from(apiKeysTable)
+      .where(eq(apiKeysTable.apiKey, apikey));
+
+    const [user] = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, record.userId));
+
+    const formData = await request.formData();
     const imageFile = formData.get("image") as File;
     const imageUrl = formData.get("imageUrl");
 
-    if (imageFile) {
-      const imageMeta = await getImageMetadataFromFile(imageFile);
+    const maxAllowedImageSize = getMaxAllowedImageSize(user.subscriptionType);
+    const pipedFormData = new FormData();
 
-      
-      return NextResponse.json({
-        imageMeta,
-      });
+    if (!imageFile && !imageUrl) {
+      return NextResponse.json(
+        {
+          error: true,
+          message: "Missing Input: Provide an Image URL or File",
+        },
+        {
+          status: 400,
+        }
+      );
     }
 
-    if (imageUrl) {
-      const imageMeta = await getImageMetadataFromUrl(imageUrl as string);
-      return NextResponse.json({
-        imageMeta,
-      });
+    const imageSource = imageFile ?? imageUrl;
+
+    const imageMeta = imageFile
+      ? await getImageMetadataFromFile(imageFile)
+      : await getImageMetadataFromUrl(imageUrl as string);
+
+    if (imageMeta.sizeInBytes > maxAllowedImageSize) {
+      return NextResponse.json(
+        {
+          error: true,
+          message: "Image exceeds the maximum allowed size.",
+        },
+        { status: 403 }
+      );
     }
 
-    return NextResponse.json(
-      {
-        error: true,
-        message: "Missing Input: Provide an Image URL or File",
+    pipedFormData.append(imageFile ? "image" : "image_url", imageSource);
+
+    const res = await fetch(`${process.env.CAPTION_API_ENDPOINT}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
       },
-      {
-        status: 400,
-      }
-    );
+      body: pipedFormData,
+    });
+
+    if (!res.ok) {
+      return NextResponse.json(
+        {
+          error: true,
+          message: "Something went wrong",
+        },
+        { status: 500 }
+      );
+    }
+
+    const resBody = await res.json();
+
+    return NextResponse.json({
+      error: false,
+      message: "Captions generated successfully!",
+      caption: resBody.description,
+    });
   } catch (e: unknown) {
     if (Number(process.env.LOGGING_LEVEL) > 0) {
       console.error(e);
