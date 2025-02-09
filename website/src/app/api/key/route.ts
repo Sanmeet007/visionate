4df4c53876ae602b720979c984a360cd5005c581
Sanmeet007 <ssanmeet007@gmail.com>
2025-02-09 10:57:24 +0530

    add doc for api key

--- website/src/app/api/key/route.ts
+++ website/src/app/api/key/route.ts
@@ -1,13 +1,25 @@
+/**
+ * API Endpoints for Managing API Keys:
+ *
+ * - **POST** `/api/key` → Generate a new API key.
+ * - **PUT** `/api/key` → Update API key settings (activate/disable).
+ * - **DELETE** `/api/key` → Delete an existing API key.
+ *
+ */
+
 import { usingAuthMiddleware } from "@/middlewares/authenticator";
 import { NextResponse } from "next/server";
 
-// Generates Api key
 export const POST = usingAuthMiddleware(async (_, user) => {
   console.log(user.emailVerified);
   return new NextResponse();
 });
 
-// Deletes Api key
+export const PUT = usingAuthMiddleware(async (_, user) => {
+  console.log(user.subscriptionType);
+  return new NextResponse();
+});
+
 export const DELETE = usingAuthMiddleware(async (_, user) => {
   console.log(user.emailVerified);
   return new NextResponse();
