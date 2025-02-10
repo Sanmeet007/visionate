8628e227292a58ee936b6c21ead649af1b7e3794
Sanmeet007 <ssanmeet007@gmail.com>
2025-02-11 00:26:47 +0530

    added razorpay method ( not implemented )

---
+++ website/src/app/(pages)/payment/page.tsx
@@ -0,0 +1,129 @@
+"use client"
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Script from "next/script";
+import { useState } from "react";
+
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
+}
+
+function Payment() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("0");
+  const [currency] = useState("INR");
+
+  const createOrderId = async () => {
+    try {
+      const response = await fetch("/api/payments/initiate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          amount: parseFloat(amount) * 100,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      return data.orderId;
+    } catch (error) {
+      console.error("There was a problem with your fetch operation:", error);
+    }
+  };
+  const processPayment = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const orderId: string = await createOrderId();
+      const options = {
+        key: process.env.key_id,
+        amount: parseFloat(amount) * 100,
+        currency: currency,
+        name: "User Name",
+        readonly: { email: true, contact: true },
+        prefill: { contact: "8146123102", email: "test@example.com" },
+        description: "description",
+        order_id: orderId,
+        handler: async function (response: {
+          razorpay_payment_id: string;
+          razorpay_order_id: string;
+          razorpay_signature: string;
+        }) {
+          const data = {
+            orderCreationId: orderId,
+            razorpayPaymentId: response.razorpay_payment_id,
+            razorpayOrderId: response.razorpay_order_id,
+            razorpaySignature: response.razorpay_signature,
+          };
+
+          const result = await fetch("/api/payments/verify", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+          });
+          const res = await result.json();
+          if (res.isOk) alert("payment succeed");
+          else {
+            alert(res.message);
+          }
+        },
+      };
+      const paymentObject = new window.Razorpay(options);
+      paymentObject.on("payment.failed", function (response: any) {
+        alert(response.error.description);
+      });
+      paymentObject.open();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Script
+        id="razorpay-checkout-js"
+        src="https://checkout.razorpay.com/v1/checkout.js"
+      />
+
+      <section className="min-h-[94vh] flex flex-col gap-6 h-14 mx-5 sm:mx-10 2xl:mx-auto 2xl:w-[1400px] items-center pt-36 ">
+        <form
+          className="flex flex-col gap-6 w-full sm:w-80"
+          onSubmit={processPayment}
+        >
+          <div className="space-y-1">
+            <label>Full name</label>
+            <input
+              type="text"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="space-y-1">
+            <label>Amount</label>
+            <div className="flex gap-2">
+              <input
+                type="number"
+                step="1"
+                min={5}
+                required
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <button type="submit">Pay</button>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Payment;
