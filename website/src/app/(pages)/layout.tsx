7822c459faf44f35a24c5b8d41c34caeb738337d
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-29 02:30:59 +0530

    updated param

--- website/src/app/(pages)/layout.tsx
+++ website/src/app/(pages)/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <UserProvider user={user}>
+        <UserProvider initialUserData={user}>
           <QueryProvider>
             <MuiLocalizationProvider>
               <AppThemeProvider>
