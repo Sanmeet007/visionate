cbabb9716fdef6e159e0cb1012fa12b6cc348681
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-29 01:43:17 +0530

    added user provider

--- website/src/app/providers/user-provider.tsx
+++ website/src/app/providers/user-provider.tsx
@@ -1,3 +1,39 @@
-export const UserProvider = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+"use client";
+import { createContext, useContext, useState } from "react";
+import { usersTable } from "@/drizzle/schema";
+import { InferSelectModel } from "drizzle-orm";
+
+type UserSchema = Omit<InferSelectModel<typeof usersTable>, "hashedPassword">;
+
+export const UserContext = createContext<UserSchema | null>(null);
+export const UpdateUserContext = createContext<React.Dispatch<
+  React.SetStateAction<UserSchema | null>
+> | null>(null);
+
+export const useUserContext = () => {
+  return useContext(UserContext);
 };
+
+export const useUserUpdaterContext = () => {
+  return useContext(UpdateUserContext);
+};
+
+const UserProvider = ({
+  children,
+  user = null,
+}: {
+  children: React.ReactNode;
+  user: UserSchema | null;
+}) => {
+  const [currentUser, setUser] = useState(user);
+
+  return (
+    <UserContext.Provider value={currentUser}>
+      <UpdateUserContext.Provider value={setUser}>
+        {children}
+      </UpdateUserContext.Provider>
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
