9367449f4f747e030b06d284b2824d841221b1f0
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-29 02:04:50 +0530

    added dynamic export tag

--- website/src/app/(pages)/layout.tsx
+++ website/src/app/(pages)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import AppProgressProvider from "./nprogress";
 import { MuiLocalizationProvider } from "../providers/mui-localization-provider";
