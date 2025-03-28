396e1559597240ecf8797cf8db45b107d70a5be2
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-29 02:31:52 +0530

    added global loader

---
+++ website/src/app/providers/global-loader-provider.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Backdrop, CircularProgress } from "@mui/material";
+import { createContext, useContext, useState } from "react";
+
+interface GlobalLoaderContextValues {
+  showLoader: () => void;
+  hideLoader: () => void;
+}
+
+const GlobalLoaderContext = createContext<GlobalLoaderContextValues>({
+  showLoader: () => {},
+  hideLoader: () => {},
+});
+
+/**
+ * Hook that returns functions to show and hide the loader
+ * - showLoader: Function to show the loader
+ * - hideLoader: Function to hide the loader
+ */
+export const useLoader = () => useContext(GlobalLoaderContext);
+
+/**
+ *
+ * Provides circular progress loader show and hide functions.
+ * Use the related context hook > `useLoader`, which returns an object with showLoader and hideLoader functions.
+ *
+ * @example
+ *
+ *  export default Component(...) {
+ *    ...
+ *    const { showLoader, hideLoader } = useLoader();
+ *    return (<>...</>)
+ *  }
+ */
+const GloablLoader = ({ children }: { children: React.ReactNode }) => {
+  const [state, setState] = useState({
+    open: false,
+  });
+  const showLoader = () => {
+    setState({ open: true });
+  };
+
+  const hideLoader = () => {
+    setState({ open: false });
+  };
+
+  return (
+    <>
+      <GlobalLoaderContext.Provider value={{ showLoader, hideLoader }}>
+        <Backdrop
+          open={state.open}
+          sx={{
+            zIndex: 1,
+          }}
+        >
+          {state.open && <CircularProgress />}
+        </Backdrop>
+        {children}
+      </GlobalLoaderContext.Provider>
+    </>
+  );
+};
+
+export default GloablLoader;
