2b1b37469ffd622752305f37a20df9b1ff2dffd4
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-30 19:09:39 +0530

    fixed api key validation

--- website/src/app/(pages)/api/validate-key/route.ts
+++ website/src/app/(pages)/api/validate-key/route.ts
@@ -1,60 +1,57 @@
 import { db } from "@/drizzle";
 import { apiKeysTable } from "@/drizzle/schema";
-import { usingJoiValidatorMiddleware } from "@/middlewares/validator";
 import { eq } from "drizzle-orm";
-import Joi from "joi";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-interface RequestData {
-  apiKey: string;
-}
 
-export const GET = usingJoiValidatorMiddleware(
-  async (_, validatedData) => {
-    const apiKey = validatedData.urlData!.apiKey;
+export const GET = async () => {
+  try {
+    const apiKey = headers().get("X-API-KEY");
 
-    try {
-      const apiKeys = await db.query.apiKeysTable.findFirst({
-        where: eq(apiKeysTable.apiKey, apiKey),
-      });
-
-      if (!apiKeys) {
-        return NextResponse.json({
-          error: true,
-          message: "Invalid api Key",
-        });
-      }
-      if (apiKeys?.isActive) {
-        return NextResponse.json({
-          error: true,
-          message: "Api key not active",
-        });
-      } else {
-        return NextResponse.json({
-          error: false,
-          message: "Api key is valid",
-        });
-      }
-    } catch (e: unknown) {
-      if (Number(process.env.LOGGING_LEVEL) > 0) {
-        console.error(e);
-      }
-
+    if (!apiKey)
       return NextResponse.json(
         {
           error: true,
-          message: "Something went wrong",
+          message: "Invalid api key",
         },
-        { status: 500 }
+        {
+          status: 400,
+        }
       );
+
+    const apiKeys = await db.query.apiKeysTable.findFirst({
+      where: eq(apiKeysTable.apiKey, apiKey),
+    });
+
+    if (!apiKeys) {
+      return NextResponse.json({
+        error: true,
+        message: "Invalid api Key",
+      });
     }
-  },
-  {
-    getDataFrom: "URL",
-    validationSchema: {
-      url: Joi.object<RequestData>({
-        apiKey: Joi.string().required().length(64),
-      }),
-    },
+    if (apiKeys?.isActive) {
+      return NextResponse.json({
+        error: true,
+        message: "Api key not active",
+      });
+    } else {
+      return NextResponse.json({
+        error: false,
+        message: "Api key is valid",
+      });
+    }
+  } catch (e: unknown) {
+    if (Number(process.env.LOGGING_LEVEL) > 0) {
+      console.error(e);
+    }
+
+    return NextResponse.json(
+      {
+        error: true,
+        message: "Something went wrong",
+      },
+      { status: 500 }
+    );
   }
-);
+};
