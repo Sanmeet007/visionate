ababb2703bade81960be0d84446833ba907fc74a
Sanmeet007 <ssanmeet007@gmail.com>
2025-03-30 20:45:15 +0530

    fixed cors issue

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -5,6 +5,26 @@ const nextConfig = {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/api/generate-caption",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-API-KEY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
