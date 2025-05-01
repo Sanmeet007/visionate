c27b740b66b87c3227bd5958ae8fb3b8f4079077
Sanmeet Singh <ssanmeet007@gmail.com>
2025-05-01 11:38:06 +0530

    change port to 8000 for mac

--- caption-model/app.py
+++ caption-model/app.py
@@ -61,4 +61,4 @@ def generate_caption_route_handler():
     return jsonify({"error": "No image provided."}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, port=int(os.environ.get("PORT", 8000)))
