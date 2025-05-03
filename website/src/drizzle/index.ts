1350851014c9f1e3c1442b1053bb146040032d0b
Sanmeet007 <ssanmeet007@gmail.com>
2025-05-03 22:29:27 +0530

    removed unused code

--- website/src/drizzle/index.ts
+++ website/src/drizzle/index.ts
@@ -2,20 +2,6 @@ import { drizzle, MySql2Database } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
 
-/**
- * Pool connection for the database connection used by Drizzle ORM
- * - Maximum connections: 10
- * - Reuses connections
- */
-const poolConnection = mysql.createPool({
-  host: process.env.DB_HOSTNAME,
-  user: process.env.DB_USERNAME,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  connectionLimit: 10,
-});
-
 // Declare a global variable to hold the connection pool
 declare global {
   var mysqlPool: mysql.Pool | undefined;
