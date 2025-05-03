e8827a3116d8826e2e4363ff373058d47536a84f
Sanmeet007 <ssanmeet007@gmail.com>
2025-05-03 20:31:25 +0530

    implemented basic stats api

--- website/src/app/(pages)/api/data/base-stats/route.ts
+++ website/src/app/(pages)/api/data/base-stats/route.ts
@@ -1,5 +1,9 @@
+import { db } from "@/drizzle";
+import { apiKeysTable, apiRequestsTable } from "@/drizzle/schema";
 import { usingAuthMiddleware } from "@/middlewares/authenticator";
 import { usingJoiValidatorMiddleware } from "@/middlewares/validator";
+import { getSubDetails } from "@/utils/user-sub-details";
+import { and, avg, count, eq, sql } from "drizzle-orm";
 import Joi from "joi";
 import { NextResponse } from "next/server";
 
@@ -7,17 +11,61 @@ interface GetRequestParams {}
 
 export const GET = usingAuthMiddleware(
   usingJoiValidatorMiddleware<GetRequestParams>(
-    (_, validationResults, user) => {
-      const {} = validationResults.urlData!;
+    async (_, validationResults, user) => {
+      try {
+        const [d1Result, d2Result, d3Result] = await Promise.all([
+          db
+            .select({
+              avgImageSize: sql`ceil(avg(image_size))`.as("avgImageSize"),
+            })
+            .from(apiRequestsTable)
+            .where(eq(apiRequestsTable.userId, user!.id))
+            .limit(1),
 
-      return NextResponse.json({});
+          db
+            .select({
+              keyCount: count(apiKeysTable.id).as("keyCount"),
+            })
+            .from(apiKeysTable)
+            .where(eq(apiKeysTable.userId, user!.id))
+            .limit(1), // Optimization: Expecting a single row
+
+          db
+            .select({
+              requestThisMonth: count(apiRequestsTable.id).as(
+                "requestThisMonth"
+              ),
+            })
+            .from(apiRequestsTable)
+            .where(
+              and(
+                eq(apiRequestsTable.userId, user!.id),
+                sql`month(\`timestamp\`) = month(current_timestamp)`
+              )
+            )
+            .limit(1),
+        ]);
+
+        const { maxRequestsPerMonth } = getSubDetails(user!);
+
+        return NextResponse.json({
+          avgImageSize: Number(d1Result[0]?.avgImageSize ?? 0),
+          keyCount: d2Result[0]?.keyCount ?? 0,
+          requestsRemaining:
+            maxRequestsPerMonth - (d3Result[0]?.requestThisMonth ?? 0),
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        return NextResponse.json(
+          { error: true, message: "Something went wrong" },
+          { status: 500 }
+        );
+      }
     },
     {
       getDataFrom: "URL",
       validationSchema: {
-        url: Joi.object({
-            
-        }),
+        url: Joi.object({}),
       },
     }
   )
