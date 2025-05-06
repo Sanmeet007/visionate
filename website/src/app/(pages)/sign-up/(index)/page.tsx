16ab366112742c95be9801d9745b3d4b4dd167e8
Sanmeet007 <ssanmeet007@gmail.com>
2025-05-06 23:21:02 +0530

    added metadata

--- website/src/app/(pages)/sign-up/(index)/page.tsx
+++ website/src/app/(pages)/sign-up/(index)/page.tsx
@@ -2,7 +2,7 @@ import SignupForm from "@/app/fragments/SignupForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign Up - Visionate",
+  title: "Sign Up | Visionate",
 };
 
 const SignUpPage = () => {
--- website/src/app/(pages)/sign-up/verify-email/page.tsx
+++ website/src/app/(pages)/sign-up/verify-email/page.tsx
@@ -1,4 +1,9 @@
 import VerifyEmailClientPage from "./client-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Verify Your Account | Visionate",
+};
 
 const VerifyEmailPage = () => {
   return (
