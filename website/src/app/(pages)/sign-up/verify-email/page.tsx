c3c7257912db70c51ab5f30b4713d2ef6d6df84a
Sanmeet007 <ssanmeet007@gmail.com>
2025-05-07 00:07:57 +0530

    fixed email verification

--- website/src/app/(pages)/sign-up/verify-email/page.tsx
+++ website/src/app/(pages)/sign-up/verify-email/page.tsx
@@ -1,11 +1,24 @@
+import { getUser } from "@/auth";
 import VerifyEmailClientPage from "./client-page";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Verify Your Account | Visionate",
 };
 
-const VerifyEmailPage = () => {
+const getUserData = async () => {
+  const user = await getUser();
+  return user;
+};
+
+const VerifyEmailPage = async () => {
+  const user = await getUserData();
+
+  if (user && user.emailVerified) {
+    return redirect(`/dashboard`);
+  }
+
   return (
     <>
       <VerifyEmailClientPage />
