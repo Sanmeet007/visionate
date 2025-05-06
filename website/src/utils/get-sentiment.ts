0421152d565669c83d5c32511202193ac43edf57
Sanmeet007 <ssanmeet007@gmail.com>
2025-05-07 00:06:03 +0530

    fixed 0 % error

--- website/src/utils/get-sentiment.ts
+++ website/src/utils/get-sentiment.ts
@@ -1,7 +1,11 @@
 export const getSentiment = (s: number, f: number) => {
+  if(s == 0 && f == 0) {
+    return "NA";
+  }
+
   const total = s + f;
   const successPercent = total === 0 ? 0 : (s / total) * 100;
-
+  
   if (successPercent >= 90) {
     return "GREAT";
   } else if (successPercent >= 70) {
