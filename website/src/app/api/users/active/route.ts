82a7fcefa13fae9defc82596447019719d7d866f
Sanmeet007 <ssanmeet007@gmail.com>
2025-05-10 12:09:20 +0530

    used try catch in update user dets

--- website/src/app/api/users/active/route.ts
+++ website/src/app/api/users/active/route.ts
@@ -1,8 +1,11 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
+import { db } from "@/drizzle";
+import { usersTable } from "@/drizzle/schema";
 import { usingAuthMiddleware } from "@/middlewares/authenticator";
 import { usingEmailVerificationMiddleware } from "@/middlewares/email-verifier";
 import { usingJoiValidatorMiddleware } from "@/middlewares/validator";
+import { eq } from "drizzle-orm";
 import Joi from "joi";
 import { NextResponse } from "next/server";
 
@@ -17,10 +20,30 @@ interface PutRequestData {
 export const PUT = usingAuthMiddleware(
   usingEmailVerificationMiddleware(
     usingJoiValidatorMiddleware<PutRequestData>(
-      (req, user) => {
-        return NextResponse.json({
-          message: "User details updated successfully",
-        });
+      async (_, validationResults, user) => {
+        try {
+          const { name } = validationResults.bodyData!;
+
+          await db
+            .update(usersTable)
+            .set({ name })
+            .where(eq(usersTable.id, user!.id));
+
+          return NextResponse.json({
+            message: "User details updated successfully",
+          });
+        } catch (error) {
+          if (Number(process.env.LOGGING_LEVEL) >= 1) {
+            console.error("Error updating user details:", error);
+          }
+          
+          return NextResponse.json(
+            {
+              message: "Failed to update user details",
+            },
+            { status: 500 }
+          );
+        }
       },
       {
         getDataFrom: "BODY",
