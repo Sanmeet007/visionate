31c3fb8a0572430a1d3d368e95f65acf7efb6c29
Sanmeet007 <ssanmeet007@gmail.com>
2025-05-12 14:04:12 +0530

    added redis env var

--- website/src/app/redis/dbclient.ts
+++ website/src/app/redis/dbclient.ts
@@ -1,10 +1,22 @@
 import { createClient } from "redis";
 
-/**
- * Database client for redis ( in memory db )
- */
-const client = createClient();
-client.on("error", (err) => console.log("Redis Client Error", err));
-await client.connect();
+declare global {
+  var redisDbClient: any;
+}
+
+const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
+
+async function getRedisClient() {
+  if (!global.redisDbClient) {
+    global.redisDbClient = createClient({
+      url: redisUrl,
+    });
+    await client.connect();
+  }
+  return global.redisDbClient;
+}
+
+const client = await getRedisClient();
+client.on("error", (err: any) => console.log("Redis Client Error", err));
 
 export { client as redisDbClient };
